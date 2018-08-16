4783f6219f489ca654cb58ace05aad6abf33ff37
Decsi Bálint <decsi@astron.hu>
2018-08-16 12:13:06 +0200

    no message

--- src/main/java/com/example/balint/szakdolgozat/activities/FriendListActivity.java
+++ src/main/java/com/example/balint/szakdolgozat/activities/FriendListActivity.java
@@ -416,7 +416,7 @@ public class FriendListActivity extends ActionBarActivity {
         if (!tcps.isRequestedyet()) {
             tcps.requestFriendList();
         } else {
-            writeFriendList();
+            writeFrfsdafsiendList();
         }
     }
 }-1
