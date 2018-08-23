4ebfba8750c64071acb5d73ed7a71837fa1dfda8
Decsi Bálint <decsi@astron.hu>
2018-08-23 18:15:46 +0200

    asddsas

--- src/main/java/com/example/balint/szakdolgozat/activities/FriendListActivity.java
+++ src/main/java/com/example/balint/szakdolgozat/activities/FriendListActivity.java
@@ -16,7 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.EditText;fsdfds
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
